bd5f588d1393e1ce62074fdc8b19b7edfa40a073
Martha Garcia <marthagm91@gmail.com>
2024-10-22 14:13:53 -0400

    Section of carts for the web

---
+++ style.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.learn-more').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('Feature details coming soon!');
+    });
+});
